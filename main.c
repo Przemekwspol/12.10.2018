0b9f4de77026219e5219645b988629e902f60ca0
Przemekwspol <44050609+Przemekwspol@users.noreply.github.com>
2018-10-12 14:26:29 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -2,31 +2,16 @@
 #include "stdio.h"
 
 int main() {
-    char znak;
-    int wartosc_calkowita;
-    double wartosc_wymierna;
-    char ciag_znakow[256];
-
-    printf("Podaj znak: ");
-    scanf("%c", &znak);
-    printf("Podaj ciąg znaków");
-    scanf("%s", ciag_znakow);
-    printf("Podaj liczbę całkowitą");
-    scanf("%i", &wartosc_calkowita);
-    printf("Podaj liczbę wymierną");
-    scanf("%lf", &wartosc_wymierna);
-
-    printf("\n");
-    printf("znak = %c. \nciag znakowy = %s.\n"
-           "liczba calkowita = %i. \nliczba wymierna = %lf",
-           znak,
-           ciag_znakow,
-           wartosc_calkowita,
-           wartosc_wymierna
-
-
-
-    );
+    int Alfa = 5;
+    float Beta = 12.45;
+    printf("Wynik: \n Alfa = %d,\t Beta = %f\n",
+           Alfa, Beta + 500);
+// Wynik:
+// Alfa = 5, Beta = 512.450000
+    char opcja = 'X';
+    char *Napis = "Opis programu.";
+    printf("Wybrano opcję %c : %31s", opcja, Napis);
+// Wybrano opcję X : Opis programu.
 
 
     return 0;

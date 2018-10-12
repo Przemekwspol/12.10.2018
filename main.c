eeb7bdd6179a750eff75821a028ea97779da619c
Przemysław Szczerba <przemyslaw.szczerba.wspol@gmail.com>
2018-10-12 14:08:19 +0200

    dodane

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
-#include <stdio.h>
+
+#include "stdio.h"
 
 int main() {
-    printf("Hello, World!\n");
+    char znak;
+    int wartosc_calkowita;
+    double wartosc_wymierna;
+    char ciag_znakow[256];
+
+    printf("Podaj znak: ");
+    scanf("%c", &znak);
+    printf("Podaj ciąg znaków");
+    scanf("%s", ciag_znakow);
+    printf("Podaj liczbę całkowitą");
+    scanf("%i", &wartosc_calkowita);
+    printf("Podaj liczbę wymierną");
+    scanf("%lf", &wartosc_wymierna);
+
+    printf("\n");
+    printf("znak = %c. \nciag znakowy = %s.\n"
+           "liczba calkowita = %i. \nliczba wymierna = %lf",
+           znak,
+           ciag_znakow,
+           wartosc_calkowita,
+           wartosc_wymierna
+
+
+
+    );
+
+
     return 0;
 }

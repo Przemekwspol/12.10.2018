05ac06c6a67178fc90826291fadf1f24f393db85
Przemekwspol <44050609+Przemekwspol@users.noreply.github.com>
2018-10-12 17:20:59 +0200

    Add files via upload

---
+++ delta.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+float a,b,c, delta;
+
+int main() {
+
+    printf("Podaj a:\n");
+    scanf("%f", &a);
+    printf("Podaj b:\n");
+    scanf("%f", &b);
+    printf("Podaj c:\n");
+    scanf("%f", &c);
+    delta = b * b - 4 * a * c;
+    printf("sadjklgnfd;v %f\n", delta);
+
+
+
+
+    return 0;
+}

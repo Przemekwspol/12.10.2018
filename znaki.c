6cd4f5477742e2aa6cc9bebd6f6bbe4b66ed4e73
Przemekwspol <44050609+Przemekwspol@users.noreply.github.com>
2018-10-12 14:12:14 +0200

    Add files via upload

---
+++ znaki.c
@@ -0,0 +1,13 @@
+#include <stdio.h> /* dolaczenie biblioteki standardowej input-output */
+/* y = f(x) */
+/*     f() => 0 */
+int main() /* funkcja glowna programu */
+/* int -> typ zwracanej wartosci (int - wartosc calkowita) */
+{
+	puts("jade na rowerze "); 
+	putchar('\r');
+puts("Pierwszy program");
+	putchar('\\'); /*wypisuje \ */
+	return 0;
+} /* klamry oznaczaja blok programu, zwykle cialo funkcji */
+

253f24ccd719d0fb19095b44112b1f534fa1a839
Przemekwspol <44050609+Przemekwspol@users.noreply.github.com>
2018-10-12 14:11:11 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,33 +1,13 @@
 
 #include "stdio.h"
 
-int main() {
+int main(){
+
     char znak;
-    int wartosc_calkowita;
-    double wartosc_wymierna;
-    char ciag_znakow[256];
 
-    printf("Podaj znak: ");
-    scanf("%c", &znak);
-    printf("Podaj ciąg znaków");
-    scanf("%s", ciag_znakow);
-    printf("Podaj liczbę całkowitą");
-    scanf("%i", &wartosc_calkowita);
-    printf("Podaj liczbę wymierną");
-    scanf("%lf", &wartosc_wymierna);
+    znak = getchar();
 
-    printf("\n");
-    printf("znak = %c. \nciag znakowy = %s.\n"
-           "liczba calkowita = %i. \nliczba wymierna = %lf",
-           znak,
-           ciag_znakow,
-           wartosc_calkowita,
-           wartosc_wymierna
-
-
-
-    );
-
+    putchar(znak);
 
     return 0;
 }
